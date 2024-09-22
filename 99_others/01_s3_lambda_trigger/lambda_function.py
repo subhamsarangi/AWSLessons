2a875033a531f957c63b624b7258248bc15a1a18
subhamsarangi <subhamsarangi2016@gmail.com>
2024-09-22 19:26:57 +0530

    restructure

---
+++ 99_others/01_s3_lambda_trigger/lambda_function.py
@@ -0,0 +1,27 @@
+import csv
+import io
+import boto3
+
+s3_client = boto3.client('s3')
+
+def lambda_function(event, context):
+    print(context, "---------------->")
+    s3_record = event['Records'][0]['s3']
+    bucket = s3_record['bucket']['name']
+    key = s3_record['object']['key']
+    print(bucket, '\n', key, "----------------->")
+
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    try:
+        data = response['Body'].read().decode('utf-8')
+        reader = csv.reader(io.StringIO(data))
+        next(reader) # skip header
+        # csv header: id,first_name,last_name,email,gender,ip_address
+        for row in reader:
+            try:
+                print(f'email {row[3]}, ip_address {row[5]}')
+            except Exception as e:
+                print(str(row))
+    except Exception as e:
+        print(str(e), "~~~~~~~~~~~~")
+    return 1
